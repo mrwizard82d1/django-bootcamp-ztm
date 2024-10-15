fffba40a9075cff3ea5cdf8b989d78529416bd87
Larry Jones <mrwizard82d1@gmail.com>
2024-10-15 15:37:06 -0500

    Create temporary index page to verify "wiring"

--- code/video/movies/views.py
+++ code/video/movies/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+def index(request):
+    """A view to test our initial application version."""
+    return HttpResponse("My Fav Movies")
