8edf8f66fa7ed1cab07c1bf4924e1ea10de8c1e4
Larry Jones <mrwizard82d1@gmail.com>
2024-11-08 11:03:18 -0600

    Rename project from triptrack to triptrak

--- code/triptrack/manage.py
+++ code/triptrack/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'triptrack.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'triptrak.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
